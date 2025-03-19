5dc06d7f4c5bd4f226e06208538a25021c63c9f8
iiuperboyii <aaronjomon24@gmail.com>
2025-03-19 09:49:10 +0530

    completed

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+const port = 6000;
+
+app.use(express.json())
+
+app.post('/registration', (req, res) => {
+  const {username, email, password, dateofbirth} = req.body;
+  if(!username){ 
+    res.json({message:"Username cannot be empty"})
+  }
+  if(!email){
+    res.json({message:"Email cannot be empty"})
+  }
+  if(password.length < 8 || password.length > 16){
+    res.json({message:"Password must be between 8 and 16 characters"})
+  }
+  dateofbirth
+  res.json({message:"User created Successfully", username, email, password, dateofbirth})
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
